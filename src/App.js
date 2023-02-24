4912e8fef407d39622db91bedd7ef5e372caf5ce
Bratalix <bratalix@gmail.com>
2023-02-24 16:26:48 +1100

    Remove section from home page

--- src/App.js
+++ src/App.js
@@ -1,63 +1,63 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Global, css } from '@emotion/core';
-import Navigation from './components/navigation';
-import HomePageWrapper from './components/homepage/home-page-wrapper';
-import OtherPageWrapper from './components/other-page-wrapper';
-import Footer from './components/footer';
-import { fetchDrupalData } from './utils/fetch-functions';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Global, css } from "@emotion/core";
+import Navigation from "./components/Navigation";
+import HomePageWrapper from "./components/homepage/home-page-wrapper";
+import OtherPageWrapper from "./components/other-page-wrapper";
+import Footer from "./components/Footer";
+import { fetchDrupalData } from "./utils/fetch-functions";
 
-require('typeface-lato');
-require('typeface-roboto-slab');
+require("typeface-lato");
+require("typeface-roboto-slab");
 
 const globalStyles = css`
-  html,
-  body {
-    margin: 0;
-    outline: 0;
-    padding: 0;
-  }
+	html,
+	body {
+		margin: 0;
+		outline: 0;
+		padding: 0;
+	}
 
-  body {
-    color: #777;
-    font-family: 'Lato', sans-serif;
-    font-weight: 300;
-    text-rendering: optimizeLegibility;
-    word-wrap: break-word;
-    font-style: normal;
-    width: 100%;
-  }
+	body {
+		color: #777;
+		font-family: "Lato", sans-serif;
+		font-weight: 300;
+		text-rendering: optimizeLegibility;
+		word-wrap: break-word;
+		font-style: normal;
+		width: 100%;
+	}
 
-  a {
-    text-decoration: none;
-    -webkit-transition: all 0.3s ease-out;
-    -moz-transition: all 0.3s ease-out;
-    -o-transition: all 0.3s ease-out;
-    transition: all 0.3s ease-out;
-    color: #c2b49a;
-  }
+	a {
+		text-decoration: none;
+		-webkit-transition: all 0.3s ease-out;
+		-moz-transition: all 0.3s ease-out;
+		-o-transition: all 0.3s ease-out;
+		transition: all 0.3s ease-out;
+		color: #c2b49a;
+	}
 
-  a:hover,
-  a:focus,
-  a:active {
-    color: #2b2b2b;
-    text-decoration: none;
-  }
+	a:hover,
+	a:focus,
+	a:active {
+		color: #2b2b2b;
+		text-decoration: none;
+	}
 
-  b,
-  strong {
-    font-weight: 700;
-  }
-  h1,
-  h2,
-  h3,
-  h4,
-  h5,
-  h6 {
-    font-family: 'Roboto Slab';
-    font-style: normal;
-    font-weight: 300;
-  }
+	b,
+	strong {
+		font-weight: 700;
+	}
+	h1,
+	h2,
+	h3,
+	h4,
+	h5,
+	h6 {
+		font-family: "Roboto Slab";
+		font-style: normal;
+		font-weight: 300;
+	}
 `;
 
 /*const sanityQuery = `*[_type == "page"][0] {
@@ -90,85 +90,75 @@ const globalStyles = css`
 }`*/
 
 export default function App() {
-  const [globalSermons, setGlobalSermons] = useState(null);
-  const [pagesData, setpagesData] = useState();
-  const [pagesFetched, setPagesFetched] = useState(false);
-  const [newslettersFetched, setnewslettersFetched] = useState(false);
-  const [newslettersData, setNewslettersData] = useState();
-  const [upcomingEventsDataFetched, setUpcomingEventDataFetched] = useState(false);
-  const [upcomingEventsData, setUpcomingEventsData] = useState(null);
+	const [globalSermons, setGlobalSermons] = useState(null);
+	const [pagesData, setpagesData] = useState();
+	const [pagesFetched, setPagesFetched] = useState(false);
+	const [newslettersFetched, setnewslettersFetched] = useState(false);
+	const [newslettersData, setNewslettersData] = useState();
+	const [upcomingEventsDataFetched, setUpcomingEventDataFetched] = useState(false);
+	const [upcomingEventsData, setUpcomingEventsData] = useState(null);
 
+	useEffect(() => {
+		if (pagesFetched === false) {
+			fetchDrupalData("all_pages", {}).then((response) => {
+				let mapped = response.map((item) => ({
+					[item.slug.toLowerCase()]: item
+				}));
+				let mappedObj = Object.assign({}, ...mapped);
+				setpagesData(mappedObj);
+				setPagesFetched(true);
+			});
+		}
+	}, [pagesFetched, pagesData]);
 
-  useEffect(() => {
-    if (pagesFetched === false) {
-      fetchDrupalData('all_pages', {}).then(response => {
-        let mapped = response.map(item => ({
-          [item.slug.toLowerCase()]: item
-        }));
-        let mappedObj = Object.assign({}, ...mapped);
-        setpagesData(mappedObj);
-        setPagesFetched(true);
-      });
-    }
-  }, [pagesFetched, pagesData]);
+	useEffect(() => {
+		if (newslettersFetched === false) {
+			fetchDrupalData("newsletter", {}).then((response) => {
+				setNewslettersData(response);
+				setnewslettersFetched(true);
+			});
+		}
+	}, [newslettersFetched, newslettersData]);
 
-  useEffect(() => {
-    if (newslettersFetched === false) {
-      fetchDrupalData('newsletter', {}).then(response => {
-        setNewslettersData(response);
-        setnewslettersFetched(true);
-      });
-    }
-  }, [newslettersFetched, newslettersData]);
+	useEffect(() => {
+		if (upcomingEventsDataFetched === false) {
+			fetchDrupalData("upcomingEvents", {}).then((response) => {
+				setUpcomingEventsData(response);
+				setUpcomingEventDataFetched(true);
+			});
+		}
+	}, [upcomingEventsDataFetched, upcomingEventsData]);
 
-  useEffect(() => {
-    if (upcomingEventsDataFetched === false) {
-      fetchDrupalData('upcomingEvents', {}).then(response => {
-        setUpcomingEventsData(response);
-        setUpcomingEventDataFetched(true);
-      });
-    }
-  }, [upcomingEventsDataFetched, upcomingEventsData]);
-
-
-  return pagesFetched === true ? (
-    <Router>
-      <Global styles={globalStyles} />
-      <Route path="*" component={Navigation} />
-      <Route
-        exact
-        path="/"
-        render={() => (
-          <HomePageWrapper
-            globalSermons={globalSermons}
-            setGlobalSermons={setGlobalSermons}
-            upcomingEventsData={upcomingEventsData}
-            setUpcomingEventsData={setUpcomingEventsData}
-          />
-        )}
-      />
-      <Route
-        path="/:path"
-        render={() => (
-          <OtherPageWrapper
-            globalSermons={globalSermons}
-            setGlobalSermons={setGlobalSermons}
-            pagesData={pagesData}
-            newslettersData={newslettersData}
-          />
-        )}
-      />
-      <Route
-        path="*"
-        render={() => (
-          <Footer
-            globalSermons={globalSermons}
-            setGlobalSermons={setGlobalSermons}
-          />
-        )}
-      />
-    </Router>
-  ) : (
-      ''
-    );
+	return pagesFetched === true ? (
+		<Router>
+			<Global styles={globalStyles} />
+			<Route path="*" component={Navigation} />
+			<Route
+				exact
+				path="/"
+				render={() => (
+					<HomePageWrapper
+						globalSermons={globalSermons}
+						setGlobalSermons={setGlobalSermons}
+						upcomingEventsData={upcomingEventsData}
+						setUpcomingEventsData={setUpcomingEventsData}
+					/>
+				)}
+			/>
+			<Route
+				path="/:path"
+				render={() => (
+					<OtherPageWrapper
+						globalSermons={globalSermons}
+						setGlobalSermons={setGlobalSermons}
+						pagesData={pagesData}
+						newslettersData={newslettersData}
+					/>
+				)}
+			/>
+			<Route path="*" render={() => <Footer globalSermons={globalSermons} setGlobalSermons={setGlobalSermons} />} />
+		</Router>
+	) : (
+		""
+	);
 }
--- src/components/homepage/home-page-wrapper.js
+++ src/components/homepage/home-page-wrapper.js
@@ -1,31 +1,32 @@
-import React from 'react';
-import slider from '../../assets/img/slider.png';
+import React from "react";
+import slider from "../../assets/img/slider.png";
 //import Carousel from '../carousel/carousel';
-import Welcome from './welcome';
-import HomePageContent from './home-page-content';
-import styled from '@emotion/styled';
+//import Welcome from "./Welcome";
+//import HomePageContent from "./home-page-content";
+import styled from "@emotion/styled";
 
-const SliderImg = styled('img')`
-  width:100%;
-  height: auto;
+const SliderImg = styled("img")`
+	width: 100%;
+	height: auto;
 `;
 
-const SliderWrapper = styled('div')`
-  text-align: center;
+const SliderWrapper = styled("div")`
+	text-align: center;
+	margin: 0;
 `;
 
 export default function HomePageWrapper({ globalSermons, setGlobalSermons, upcomingEventsData }) {
-  return (
-    <section>
-      <SliderWrapper>
-        <SliderImg className="img-responsive img-full-width" src={slider} width="1440" height="600" />
-      </SliderWrapper>
-      <Welcome />
+	return (
+		<section>
+			<SliderWrapper>
+				<SliderImg className="img-responsive img-full-width" src={slider} width="1440" height="600" />
+			</SliderWrapper>
+			{/* <Welcome />
       <HomePageContent
         globalSermons={globalSermons}
         setGlobalSermons={setGlobalSermons}
         upcomingEventsData={upcomingEventsData}
-      />
-    </section>
-  );
+      /> */}
+		</section>
+	);
 }
